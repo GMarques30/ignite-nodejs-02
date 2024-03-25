8bd42bb1e6109f43343b61c15f25393ed70a6691
Giovanni Marques <giovanni.marques30@hotmail.com>
2024-03-24 22:21:01 -0300

    fix: fix create user either

--- src/domain/users/application/use-cases/create-user.ts
+++ src/domain/users/application/use-cases/create-user.ts
@@ -1,4 +1,4 @@
-import { left, right } from 'src/core/either'
+import { Either, left, right } from 'src/core/either'
 import { UserRepository } from '../repositories/user-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { HashGenerator } from '../hasher/hash-generator'
@@ -10,13 +10,24 @@ interface CreateUserInput {
   password: string
 }
 
+type CreateUserOutput = Either<
+  UserAlreadyExistsError,
+  {
+    user: User
+  }
+>
+
 export class CreateUserUseCase {
   constructor(
     private readonly userRepository: UserRepository,
     private readonly hashGenerator: HashGenerator
   ) {}
 
-  async execute({ name, email, password }: CreateUserInput) {
+  async execute({
+    name,
+    email,
+    password
+  }: CreateUserInput): Promise<CreateUserOutput> {
     const userAlreadyExists = await this.userRepository.findByEmail(email)
 
     if (userAlreadyExists) {
